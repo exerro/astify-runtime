7301c6aa6d2e639f9135dced175f390582d9265c
exerro <benedict.allen2514@gmail.com>
2018-07-15 18:12:05 +0100

    Implement equals() in Token class

--- token/Token.java
+++ token/Token.java
@@ -3,6 +3,8 @@ package astify.token;
 import astify.core.Position;
 import astify.core.Positioned;
 
+import static java.util.Objects.hash;
+
 public class Token implements Positioned {
     private final TokenType type;
     private final String value;
@@ -37,5 +39,15 @@ public class Token implements Positioned {
     @Override public String toString() {
         if (type == TokenType.EOF) return "<EOF>";
         return type.toString() + " \"" + value.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n") + "\"";
+    }
+
+    @Override public boolean equals(Object other) {
+        if (!(other instanceof Token)) return false;
+        Token otherCasted = (Token) other;
+        return type == otherCasted.type && value.equals(otherCasted.value);
+    }
+
+    @Override public int hashCode() {
+        return hash(type, value);
     }
 }
